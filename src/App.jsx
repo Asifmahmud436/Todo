d29a4073f660d89e9c4351e301bdc2a13ff721fb
Zettex <safaandsafa4@gmail.com>
2024-12-14 01:34:40 +0600

    app initialized

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,37 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  function handleForm(formData) {
+    console.log("form is working");
+  }
   return (
     <>
-      <h1>Hello world</h1>
+      {/* Form to create a task */}
+      <form action="handleForm">
+        
+        <label htmlFor="task">
+          Task
+          <input type="text" name="Task" id="task" />
+        </label>
+        <br />
+
+        {/* Priority radio buttons to arrange tasks */}
+        <label htmlFor="important">
+          Important
+          <input type="radio" name="task_priority" id="important" value='important'/>
+        </label>
+        <br />
+        
+        <label htmlFor="normal">
+          Normal
+          <input type="radio"
+           name="task_priority" 
+           id="normal" 
+           value='important' />
+
+        </label>
+        <button>Add</button>
+      </form>
     </>
   );
 }
