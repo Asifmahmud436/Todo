b323c75889a8bf41888a02b504b080879695ae64
Zettex <safaandsafa4@gmail.com>
2024-12-14 22:17:41 +0600

    imported and installed nanoid

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,51 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { nanoid } from "nanoid";
 
 function App() {
-  function handleForm(formData) {
-    console.log("form is working");
+  const [tasks, setTasks] = useState([]);
+  function handleForm(FormData) {
+    const taskInfo = FormData.get("task");
+    setTasks((prev) => [...prev, taskInfo]);
   }
+  let taskArray = tasks.map((task) => <p key={nanoid()}>{task}</p>);
+
   return (
     <>
       {/* Form to create a task */}
       <form action="handleForm">
-        
         <label htmlFor="task">
           Task
-          <input type="text" name="Task" id="task" />
+          <input type="text" name="task" id="task" />
         </label>
         <br />
 
         {/* Priority radio buttons to arrange tasks */}
         <label htmlFor="important">
           Important
-          <input type="radio" name="task_priority" id="important" value='important'/>
+          <input
+            type="radio"
+            name="task_priority"
+            id="important"
+            value="important"
+          />
         </label>
-        <br />
-        
+
         <label htmlFor="normal">
           Normal
-          <input type="radio"
-           name="task_priority" 
-           id="normal" 
-           value='important' />
-
+          <input
+            type="radio"
+            name="task_priority"
+            id="normal"
+            value="important"
+          />
         </label>
         <button>Add</button>
       </form>
+      <div>{taskArray}</div>
     </>
   );
 }
