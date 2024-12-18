2d15853a6e5f36900eef115b447ca8910693cad9
Zettex <safaandsafa4@gmail.com>
2024-12-18 13:28:14 +0600

    finished the task design

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,10 +1,14 @@
 export default function Task(props){
     return(
         <div className="task">
-            <h2 className={props.isComplete ? 'task-done':''}>{props.work}</h2>
-            <p className={props.priority=='important' ? 'task-red':'task-yellow'}>{props.priority}</p>
-            <button onClick={props.delete}>Delete</button>
-            <button onClick={props.finished}>Complete</button>
+            <div>
+                <h2 className={props.isComplete ? 'task-done':''}>{props.work}</h2>
+                <p className={props.priority=='important' ? 'task-red':'task-yellow'}>{props.priority}</p>
+            </div>
+            <div className="task-btn">
+                <button className="delete" onClick={props.delete}>Delete</button>
+                <button className="complete" onClick={props.finished}>Complete</button>
+            </div>
         </div>
     )
 }
